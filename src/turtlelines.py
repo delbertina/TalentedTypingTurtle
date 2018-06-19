508871eef0375fb98587e456b9505af91a76d295
delbertina <delbertina96@gmail.com>
2018-06-18 22:19:44 -0500

    Added helper functions to trim down letters file

--- src/turtlelines.py
+++ src/turtlelines.py
@@ -19,3 +19,35 @@ def back_move(distance):
     turtle.right(180)
     turtle.forward(distance)
 
+
+def left_move_double(distance1, distance2=None):
+    left_move(distance1)
+    if distance2 is None:
+        left_move(distance1)
+    else:
+        left_move(distance2)
+
+
+def right_move_double(distance1, distance2=None):
+    right_move(distance1)
+    if distance2 is None:
+        right_move(distance1)
+    else:
+        right_move(distance2)
+
+
+def left_right_move_double(distance1, distance2=None):
+    left_move(distance1)
+    if distance2 is None:
+        right_move(distance1)
+    else:
+        right_move(distance2)
+
+
+def right_left_move_double(distance1, distance2=None):
+    right_move(distance1)
+    if distance2 is None:
+        left_move(distance1)
+    else:
+        left_move(distance2)
+
