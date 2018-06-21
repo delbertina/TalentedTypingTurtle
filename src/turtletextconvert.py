db04a6b4f3feb14266a687f9d36bb63facf8036e
delbertina <delbertina96@gmail.com>
2018-06-20 22:43:04 -0500

    Reworked main turtletextconvert file into methods and cleaned redundant code

--- src/turtletextconvert.py
+++ src/turtletextconvert.py
@@ -4,80 +4,93 @@
 import turtle
 import turtleletters
 
+
+def calc_start(letter_input):
+    if len(letter_input) > 1:
+        gaps = len(letter_input) - 1
+        letters = len(letter_input)
+        starting_distance = -1 * (((gaps + letters) * 50) / 2)
+        if abs(starting_distance) > (turtle.window_width() / 2):
+            starting_distance = 0 - ((turtle.window_width / 2) - 25)
+    else:
+        starting_distance = -25
+    print "Starting distance: " + str(starting_distance)
+    return starting_distance
+
+
+def draw_text(letter_input, starting_distance):
+    letter_upper = letter_input.upper()
+    letter_list = [letter_upper]
+
+    turtle.penup()
+    turtle.pensize(5)
+    turtle.goto(starting_distance, 0)
+
+    for i in range(len(letter_upper)):
+        if letter_list[0][i] == 'A':
+            turtleletters.a()
+        if letter_list[0][i] == 'B':
+            turtleletters.b()
+        if letter_list[0][i] == 'C':
+            turtleletters.c()
+        if letter_list[0][i] == 'D':
+            turtleletters.d()
+        if letter_list[0][i] == 'E':
+            turtleletters.e()
+        if letter_list[0][i] == 'F':
+            turtleletters.f()
+        if letter_list[0][i] == 'G':
+            turtleletters.g()
+        if letter_list[0][i] == 'H':
+            turtleletters.h()
+        if letter_list[0][i] == 'I':
+            turtleletters.i()
+        if letter_list[0][i] == 'J':
+            turtleletters.j()
+        if letter_list[0][i] == 'K':
+            turtleletters.k()
+        if letter_list[0][i] == 'L':
+            turtleletters.l()
+        if letter_list[0][i] == 'M':
+            turtleletters.m()
+        if letter_list[0][i] == 'N':
+            turtleletters.n()
+        if letter_list[0][i] == 'O':
+            turtleletters.o()
+        if letter_list[0][i] == 'P':
+            turtleletters.p()
+        if letter_list[0][i] == 'Q':
+            turtleletters.q()
+        if letter_list[0][i] == 'R':
+            turtleletters.r()
+        if letter_list[0][i] == 'S':
+            turtleletters.s()
+        if letter_list[0][i] == 'T':
+            turtleletters.t()
+        if letter_list[0][i] == 'U':
+            turtleletters.u()
+        if letter_list[0][i] == 'V':
+            turtleletters.v()
+        if letter_list[0][i] == 'W':
+            turtleletters.w()
+        if letter_list[0][i] == 'X':
+            turtleletters.x()
+        if letter_list[0][i] == 'Y':
+            turtleletters.y()
+        if letter_list[0][i] == 'Z':
+            turtleletters.z()
+        if letter_list[0][i] == ' ':
+            turtle.forward(50)
+
+        i += i
+
+
 # Get user input
 letterInput = raw_input("Enter a letter: ")
 
 # determine starting distance
-if len(letterInput) > 1:
-    gaps = len(letterInput) - 1
-    letters = len(letterInput)
-    startingDistance = -1 * (((gaps + letters) * 50) / 2)
-else:
-    startingDistance = -25
+startingDistance = calc_start(letterInput)
 
-letterInput = letterInput.upper()
-letterList = [letterInput]
+draw_text(letterInput, startingDistance)
+turtle.exitonclick()
 
-i = 0
-
-turtle.penup()
-turtle.pensize(5)
-turtle.goto(startingDistance, 0)
-
-for i in range(len(letterInput)):
-    if letterList[0][i] == 'A':
-        turtleletters.a()
-    if letterList[0][i] == 'B':
-        turtleletters.b()
-    if letterList[0][i] == 'C':
-        turtleletters.c()
-    if letterList[0][i] == 'D':
-        turtleletters.d()
-    if letterList[0][i] == 'E':
-        turtleletters.e()
-    if letterList[0][i] == 'F':
-        turtleletters.f()
-    if letterList[0][i] == 'G':
-        turtleletters.g()
-    if letterList[0][i] == 'H':
-        turtleletters.h()
-    if letterList[0][i] == 'I':
-        turtleletters.i()
-    if letterList[0][i] == 'J':
-        turtleletters.j()
-    if letterList[0][i] == 'K':
-        turtleletters.k()
-    if letterList[0][i] == 'L':
-        turtleletters.l()
-    if letterList[0][i] == 'M':
-        turtleletters.m()
-    if letterList[0][i] == 'N':
-        turtleletters.n()
-    if letterList[0][i] == 'O':
-        turtleletters.o()
-    if letterList[0][i] == 'P':
-        turtleletters.p()
-    if letterList[0][i] == 'Q':
-        turtleletters.q()
-    if letterList[0][i] == 'R':
-        turtleletters.r()
-    if letterList[0][i] == 'S':
-        turtleletters.s()
-    if letterList[0][i] == 'T':
-        turtleletters.t()
-    if letterList[0][i] == 'U':
-        turtleletters.u()
-    if letterList[0][i] == 'V':
-        turtleletters.v()
-    if letterList[0][i] == 'W':
-        turtleletters.w()
-    if letterList[0][i] == 'X':
-        turtleletters.x()
-    if letterList[0][i] == 'Y':
-        turtleletters.y()
-    if letterList[0][i] == 'Z':
-        turtleletters.z()
-    if letterList[0][i] == ' ':
-        turtle.forward(50)
-
-    i += i
